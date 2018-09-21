d07e2ac7a86c888a1bf392c963a99c804c15030d
Nikita <nikita-bat2000@mail.ru>
2018-09-21 11:18:50 +0500

    added split

--- lamdafunc/lamdafunc.py
+++ lamdafunc/lamdafunc.py
@@ -1,6 +1,4 @@
-a = []
-for i in range(int(input("Введите размерность списка: "))):
-    a.append(int(input(str(i + 1)+") ")))
+a = list(map(lambda x: int(x), input("Input list through space\n").split(" ")))
 b = list(map(lambda x: x * 2, list(filter(lambda x: x > 5 , a))))
 try:
     print("Max:", max(b), "\nSum:", sum(b))
