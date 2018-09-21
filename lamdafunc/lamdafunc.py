902908ede122cffcbc81b487ce2cbc94744645c5
Nikita <nikita-bat2000@mail.ru>
2018-09-21 11:25:04 +0500

    added руссский

--- lamdafunc/lamdafunc.py
+++ lamdafunc/lamdafunc.py
@@ -1,4 +1,4 @@
-a = list(map(lambda x: int(x), input("Input list through space\n").split(" ")))
+a = list(map(lambda x: int(x), input("Введите элементы списка через пробел\n").split(" ")))
 b = list(map(lambda x: x * 2, list(filter(lambda x: x > 5 , a))))
 try:
     print("Max:", max(b), "\nSum:", sum(b))
